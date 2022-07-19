2eb66df0c6c2941a7db56178cad5623764d4e9c7
Jim.Jiang <jim.jiang@ringcentral.com>
2022-07-19 17:19:34 +0800

    first commit.

---
+++ MonitorPhysicalSize/MonitorPhysicalSizeCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management;
+
+namespace MonitorPhysicalSizeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"\root\wmi", @"SELECT * FROM WmiMonitorBasicDisplayParams");
+
+            foreach (ManagementObject managementObject in searcher.Get())
+            {
+                double width = (byte)managementObject["MaxHorizontalImageSize"];
+                double height = (byte)managementObject["MaxVerticalImageSize"];
+
+                Console.WriteLine($"Monitor Size: width:{width}cm height:{height}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
